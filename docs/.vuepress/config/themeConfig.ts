ddecda32c352f471021df76e1cfdfbac628ca2c6
KaibutsuSama <XINLEROY.LIU001@STU.BMCC.CUNY.EDU>
2022-09-14 10:41:23 -0400

    Update themeConfig.ts

--- docs/.vuepress/config/themeConfig.ts
+++ docs/.vuepress/config/themeConfig.ts
@@ -45,7 +45,7 @@ export default  <VdoingThemeConfig> {
 
 
     bodyBgImg: [
-        'https://s3.bmp.ovh/imgs/2022/09/12/0972d3c296f23225.jpg',
+        'https://img-baofun.zhhainiao.com/fs/c80f922628e1baca2ce1a003289b091a.jpg',
     ], // body背景大图，默认无。 单张图片 String | 多张图片 Array, 多张图片时隔bodyBgImgInterval切换一张。
     bodyBgImgOpacity: 0.5, // body背景图透明度，选值 0.1~1.0, 默认0.5
     bodyBgImgInterval: 15, // body多张背景图时的切换间隔, 默认15，单位s

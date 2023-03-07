ca8387e30b3a761089b257431e8bc2b95c71c176
KaibutsuSama <XINLEROY.LIU001@STU.BMCC.CUNY.EDU>
2023-03-07 10:47:39 -0500

    update

--- docs/.vuepress/common/nav.ts
+++ docs/.vuepress/common/nav.ts
@@ -1,25 +1,11 @@
 // @ts-ignore
-import record from '../nav/recordNav'
-// @ts-ignore
-import recordNav from '../nav/recordNav'
-// @ts-ignore
-import databaseNav from '../nav/databaseNav'
-// @ts-ignore
-import toolsNav from '../nav/toolsNav'
-// @ts-ignore
-import frontendNav from '../nav/frontendNav'
+import blogNav from '../nav/blogNav'
 
 export default [
     {
         text: '首页',link: '/'
     },
 
-    recordNav, //开发小记
-
-    frontendNav, // 前端
-
-    databaseNav,//数据库
-
-    toolsNav,//工具
+    blogNav,// 心情小记
 
 ]
--- docs/.vuepress/nav/recordNav.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    text: '🍓开发小记', link: '/record/',
-    items: [
-        { text: 'Java', link: '/java/' },
-    ],
-}

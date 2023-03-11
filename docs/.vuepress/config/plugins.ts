250d454198dc74804fc34227e639dac010a9d375
KaibutsuSama <XINLEROY.LIU001@STU.BMCC.CUNY.EDU>
2023-03-11 12:59:45 -0500

    Update plugins.ts

--- docs/.vuepress/config/plugins.ts
+++ docs/.vuepress/config/plugins.ts
@@ -52,35 +52,35 @@ export default <UserPlugins> [
             },
         },
     ],
-    // [
-    //   'vuepress-plugin-zooming', // 放大图片
-    //   {
-    //     selector: '.theme-vdoing-content img:not(.no-zoom)', // 排除class是no-zoom的图片
-    //     options: {
-    //       bgColor: 'rgba(0,0,0,0.6)',
-    //     },
-    //   },
-    // ],
-    // [
-    //   'vuepress-plugin-comment', // 评论
-    //   {
-    //     choosen: 'gitalk',
-    //     options: {
-    //       clientID: 'a6e1355287947096b88b',
-    //       clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-    //       repo: 'Kaibutsu-Blog', // GitHub 仓库
-    //       owner: 'KaibutsuSama', // GitHub仓库所有者
-    //       admin: ['KaibutsuSama'], // 对仓库有写权限的人
-    //       // distractionFreeMode: true,
-    //       pagerDirection: 'last', // 'first'正序 | 'last'倒序
-    //       id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
-    //       title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
-    //       labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
-    //       body:
-    //         '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
-    //     },
-    //   },
-    // ],
+    [
+      'vuepress-plugin-zooming', // 放大图片
+      {
+        selector: '.theme-vdoing-content img:not(.no-zoom)', // 排除class是no-zoom的图片
+        options: {
+          bgColor: 'rgba(0,0,0,0.6)',
+        },
+      },
+    ],
+    [
+      'vuepress-plugin-comment', // 评论
+      {
+        choosen: 'gitalk',
+        options: {
+          clientID: 'a6e1355287947096b88b',
+          clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
+          repo: 'Kaibutsu-Blog', // GitHub 仓库
+          owner: 'KaibutsuSama', // GitHub仓库所有者
+          admin: ['KaibutsuSama'], // 对仓库有写权限的人
+          // distractionFreeMode: true,
+          pagerDirection: 'last', // 'first'正序 | 'last'倒序
+          id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
+          title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
+          labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
+          body:
+            '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
+        },
+      },
+    ],
     [
         '@vuepress/last-updated', // "上次更新"时间格式
         {
